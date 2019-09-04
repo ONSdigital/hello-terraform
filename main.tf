93a5b189b9825ab7e1e026f045d74e99a4708afb
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2019-09-04 12:08:52 +0100

    Remove the need to provide credentials directly to TF

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@
 # PROVIDERS
 ##################################################################################
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
   region     = "eu-west-2"
   version    = "~> 2.23.0"
 }
--- variables.tf
+++ variables.tf
@@ -2,13 +2,6 @@
 # VARIABLES
 ##################################################################################
 
-variable "aws_access_key" {
-  description = "AWS Access Key"
-}
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-}
-
 variable "private_key_path" {
   description = "Path to the private key used to SSH into the EC2 instance"
 }
